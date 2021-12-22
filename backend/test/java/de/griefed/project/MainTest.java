4d90a0baa5b45b6e6555e8262f0cf050960d3724
Griefed <griefed@griefed.de>
2021-12-22 20:18:11 +0100

    no message

---
+++ backend/test/java/de/griefed/project/MainTest.java
@@ -0,0 +1,16 @@
+package de.griefed.project;
+
+import org.junit.jupiter.api.Test;
+
+public class MainTest {
+
+    MainTest() {
+
+    }
+
+    @Test
+    void testMain() {
+        Main.main(null);
+    }
+
+}
